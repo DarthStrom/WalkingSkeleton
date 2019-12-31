131653336827b958d2765eb1431aa666879dde0c
Jason Duffy <jasonsduffy@gmail.com>
2019-12-31 12:56:44 -0500

    bring in winit for window drawing

--- src/main_win32.rs
+++ src/main_win32.rs
@@ -1,4 +1,44 @@
+use winit::{ControlFlow, Event::WindowEvent, WindowEvent::*};
+
 pub fn main() {
-    // Casey added a message box - skipping that because printing is easier for now
-    println!("windows!");
+    let mut events_loop = winit::EventsLoop::new();
+    let _window = winit::Window::new(&events_loop).expect("could not create window");
+
+    events_loop.run_forever(|event| match event {
+        // wm_size
+        WindowEvent {
+            event: Resized(logical_size),
+            ..
+        } => {
+            println!("wm_size: {:?}", logical_size);
+            ControlFlow::Continue
+        }
+        // wm_destroy
+        WindowEvent {
+            event: Destroyed, ..
+        } => {
+            println!("wm_destroy");
+            ControlFlow::Break
+        }
+        // wm_close
+        WindowEvent {
+            event: CloseRequested,
+            ..
+        } => {
+            println!("wm_close");
+            ControlFlow::Break
+        }
+        // wm_activateapp
+        WindowEvent {
+            event: Focused(gained_focus),
+            ..
+        } => {
+            println!("wm_activateapp: gained focus: {}", gained_focus);
+            ControlFlow::Continue
+        }
+        _ => {
+            println!("default");
+            ControlFlow::Continue
+        }
+    });
 }

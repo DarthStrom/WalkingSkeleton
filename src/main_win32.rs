acf48b9113ad8bb0522dd2dda4c2f510a70d9ae2
Jason Duffy <jasonsduffy@gmail.com>
2020-01-01 10:26:59 -0500

    Day 002: Opening a Win32 Window

--- src/main_win32.rs
+++ src/main_win32.rs
@@ -1,8 +1,22 @@
-use winit::{ControlFlow, Event::WindowEvent, WindowEvent::*};
+use winapi::{
+    shared::windef::HWND,
+    um::{
+        wingdi::{PatBlt, WHITENESS},
+        winuser,
+    },
+};
+use winit::{
+    os::windows::WindowExt, ControlFlow, Event::WindowEvent, WindowBuilder, WindowEvent::*,
+};
+
+const WINDOW_NAME: &str = "Walking Skeleton";
 
 pub fn main() {
     let mut events_loop = winit::EventsLoop::new();
-    let _window = winit::Window::new(&events_loop).expect("could not create window");
+    let window = WindowBuilder::new()
+        .with_title(WINDOW_NAME)
+        .build(&events_loop)
+        .expect("could not create window");
 
     events_loop.run_forever(|event| match event {
         // wm_size
@@ -10,6 +24,9 @@ pub fn main() {
             event: Resized(logical_size),
             ..
         } => {
+            // TODO: are you supposed to repaint here as well?
+            // when the window is resized we don't seem to be
+            // getting a refresh event
             println!("wm_size: {:?}", logical_size);
             ControlFlow::Continue
         }
@@ -36,9 +53,21 @@ pub fn main() {
             println!("wm_activateapp: gained focus: {}", gained_focus);
             ControlFlow::Continue
         }
-        _ => {
-            println!("default");
+        WindowEvent { event: Refresh, .. } => {
+            let mut paint = winuser::PAINTSTRUCT::default();
+            let hwnd = window.get_hwnd() as HWND;
+            unsafe {
+                let device_context = winuser::BeginPaint(hwnd, &mut paint);
+                let x = paint.rcPaint.left;
+                let y = paint.rcPaint.top;
+                let width = paint.rcPaint.right - paint.rcPaint.left;
+                let height = paint.rcPaint.bottom - paint.rcPaint.top;
+                PatBlt(device_context, x, y, width, height, WHITENESS);
+                winuser::EndPaint(hwnd, &paint);
+            }
+
             ControlFlow::Continue
         }
+        _ => ControlFlow::Continue,
     });
 }
